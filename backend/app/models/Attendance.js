21efbe3aec6dd8d32e115093b0d25050f1cc87e1
erakercuku <149300252+erakercuku@users.noreply.github.com>
2025-03-05 15:24:50 +0100

    Delete backend

--- backend/app/models/Attendance.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AttendanceSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
-    date: { type: Date, required: true },
-    status: { type: String, enum: ['Present', 'Absent'], required: true },
-});
-
-module.exports = mongoose.model('Attendance', AttendanceSchema);
