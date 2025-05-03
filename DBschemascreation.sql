33c1b68edfa454ab705a30862e80a6746a35da10
1zzuk1 <94109681+1zzuk1@users.noreply.github.com>
2025-05-03 13:56:07 +0300

    Update DBschemascreation.sql

--- DBschemascreation.sql
+++ DBschemascreation.sql
@@ -1,29 +1,47 @@
--- Table: Users
 CREATE TABLE Users (
-    id CHAR(16) NOT NULL,
+    id CHAR(16) PRIMARY KEY,
     registration_date DATETIME NOT NULL,
     account_status ENUM('ACTIVE', 'DEACTIVATED') NOT NULL,
-    roles ENUM('Admin', 'User', 'Moderator') NOT NULL,
-    PRIMARY KEY (id)
+    roles ENUM('Admin', 'User', 'Moderator') NOT NULL
 );
 
--- Table: user_data
-CREATE TABLE user_data (
-    id CHAR(16) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    surname VARCHAR(255) NOT NULL,
-    course VARCHAR(255),
-    position VARCHAR(255),
-    PRIMARY KEY (id),
-    CONSTRAINT fk_user_data_id FOREIGN KEY (id) REFERENCES Users(id)
+CREATE TABLE UserData (
+    id CHAR(16) PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    surname VARCHAR(100) NOT NULL,
+    course VARCHAR(100),      -- NULL if not university
+    position VARCHAR(100),    -- NULL if not work
+    FOREIGN KEY (id) REFERENCES Users(id)
 );
 
--- Table: user_actions
-CREATE TABLE user_actions (
+CREATE TABLE Modules (
+    module_id CHAR(8) PRIMARY KEY,
+    module_name VARCHAR(255) NOT NULL,
+    module_type ENUM('Course', 'Work') NOT NULL
+);
+
+CREATE TABLE ModuleSchedule (
+    schedule_id VARCHAR(20) PRIMARY KEY,
+    module_id CHAR(8) NOT NULL,
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    FOREIGN KEY (module_id) REFERENCES Modules(module_id)
+);
+
+CREATE TABLE UserModules (
     id CHAR(16) NOT NULL,
+    module_id CHAR(8) NOT NULL,
+    PRIMARY KEY (id, module_id),
+    FOREIGN KEY (id) REFERENCES Users(id),
+    FOREIGN KEY (module_id) REFERENCES Modules(module_id)
+);
+
+CREATE TABLE UserActions (
+    id CHAR(16) NOT NULL,
+    module_id CHAR(8) NOT NULL,
     action ENUM('CHECKIN', 'CHECKOUT') NOT NULL,
     time DATETIME NOT NULL,
-    -- Using a composite primary key in case a user has multiple actions at different times.
     PRIMARY KEY (id, time),
-    CONSTRAINT fk_user_actions_id FOREIGN KEY (id) REFERENCES Users(id)
+    FOREIGN KEY (id) REFERENCES Users(id),
+    FOREIGN KEY (module_id) REFERENCES Modules(module_id)
 );
