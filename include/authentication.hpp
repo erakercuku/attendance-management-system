be5a055ee5e54a46cadc43af7f9044d6d5ba4b4d
haziel <144873984+itshazi@users.noreply.github.com>
2025-05-07 14:54:26 -0600

    Update authentication.hpp
    moved login and create_account logic to Auth class

--- include/authentication.hpp
+++ include/authentication.hpp
@@ -1,12 +1,17 @@
 #ifndef AUTHENTICATION_HPP
 #define AUTHENTICATION_HPP
 
+#include "user.hpp"
+#include <memory>
 #include <string>
 
 class Auth 
 {
 public:
-    static std::string hash_password(const std::string &password);
+    static std::unique_ptr<User> login(void);
+    static std::unique_ptr<User> create_account(void);
+
+    static const std::string hash(const std::string &original);
     static bool verify_password(const std::string &inp_password, const std::string &hashed_password);
 };
 
