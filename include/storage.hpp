f22460306af6281e46070d40787dcfe8e811da65
haziel <144873984+itshazi@users.noreply.github.com>
2025-05-08 02:24:48 -0600

    Update storage.hpp
    added various methods to save and load data to the database

--- include/storage.hpp
+++ include/storage.hpp
@@ -8,8 +8,15 @@ class Storage
 private:
     static constexpr const char* USER_FILE = "data/users.csv";
     static constexpr const char* ATTENDANCE_FILE = "data/attendance.csv";
+    static constexpr const char* HASHED_ID_FILE = "data/hashed_id.csv";
 
 public:
+    static void save_user_to_db(const User &user);
+    static bool load_user_from_db(const std::string &username, Str_pair &fullname, std::string &password, std::string &ID, Roles &role);
+    static void load_users_from_db(void);
+    static void save_attendance_to_db(void);
+    static void load_attendance_from_db(void);
+
     static bool save_user(const User &user);
     static bool load_user(const std::string &inp_username, Str_pair &fullname, std::string &password, std::string &ID, Roles &role);
     static bool save_user_attendance(const UserAttendance &user_att);
@@ -19,6 +26,8 @@ public:
     static bool load_all_users(void);
     static bool save_attendance(void);
     static bool load_attendance(void);
+
+    static void save_qr_id_to_csv(const std::string &id);
 };
 
 #endif
