0cf87573117dc12936975d5ed2963256e33310a8
haziel <144873984+itshazi@users.noreply.github.com>
2025-05-10 08:47:43 -0600

    Update input.hpp
    added some new methods for date and log time validation

--- include/input.hpp
+++ include/input.hpp
@@ -18,6 +18,8 @@ public:
     static bool valid_name(const std::string &name);
     static bool valid_username(const std::string &username);
     static bool valid_password(const std::string &password);
+    static bool valid_date(const std::string &date);
+    static bool valid_log_time(const std::string &log_time);
 
     static bool is_username_unique(const std::string &username);
 };
