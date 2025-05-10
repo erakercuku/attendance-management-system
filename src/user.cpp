f464cad0b998a9378ead8131d748a23a828681af
haziel <144873984+itshazi@users.noreply.github.com>
2025-05-10 08:38:08 -0600

    Update user.cpp
    Added a lot of the missing methods for all roles and updated certain logic with the menus

--- src/user.cpp
+++ src/user.cpp
@@ -39,7 +39,7 @@ void User::display_menu(void)
     }
 }
 
-void Manager::display_menu()
+void Manager::display_menu(void)
 {
     const int options = MANAGER_OPT_COUNT - 1;
     std::string input;
@@ -74,7 +74,7 @@ void Manager::display_menu()
     }
 }
 
-void Admin::display_menu()
+void Admin::display_menu(void)
 {
     const int options = ADMIN_OPT_COUNT - 1;
     std::string input;
@@ -120,40 +120,175 @@ void Admin::display_menu()
 
 void User::view_attendance(void) const
 {
-    std::cout << "you are viewing the attendance.\n";
-    return;
+    std::cout << "Your Attendance Log:\n";
+
+    auto it = std::find_if(attendance.begin(), attendance.end(),
+        [this](const UserAttendance &ua)
+        {
+            return ua.user->get_username() == this->username;
+        });
+
+    if (it == attendance.end())
+    {
+        std::cout << "No attendance record found.\n";
+        return;
+    }
+
+    for (const auto &[date, log] : it->logs)
+    {
+        std::cout << "\nDate: " << date << '\n';
+        std::cout << "  Login Time:     " << log.login_time << '\n';
+        std::cout << "  Logout Time:    " << log.logout_time << '\n';
+        std::cout << "  Expected Login: " << log.expected_login << '\n';
+        std::cout << "  Expected Logout:" << log.expected_logout << '\n';
+    }
 }
 
 void Manager::view_attendance(void) const
 {
-    std::cout << "you are viewing the attendance.\n";
-    return;
-}
+    std::cout << "Viewing all users' attendance logs:\n";
 
-void Admin::view_attendance(void) const
-{
-    std::cout << "you are viewing the attendance.\n";
-    return;
+    for (const auto &ua : attendance)
+    {
+        std::cout << "\nUser: " << ua.user->get_username() << '\n';
+
+        for (const auto &[date, log] : ua.logs)
+        {
+            std::cout << "  Date: " << date << '\n';
+            std::cout << "    Login Time:     " << log.login_time << '\n';
+            std::cout << "    Logout Time:    " << log.logout_time << '\n';
+            std::cout << "    Expected Login: " << log.expected_login << '\n';
+            std::cout << "    Expected Logout:" << log.expected_logout << '\n';
+        }
+    }
 }
 
 void Manager::get_analytics(void) const
 {
-    std::cout << "you are getting the analytics.\n";
-    return;
+    std::cout << "[Linking to R analytics interface...]\n";
+    std::cout << "Opening external dashboard for attendance reports.\n";
 }
 
 void Admin::edit_attendance(void)
 {
-    std::cout << "you are editing the attendance.\n";
-    return;
+    std::string username;
+
+    Input::get("Enter the username of the user to edit their attendance: ", username, USERNAME_MAX, Input::valid_username);
+
+    auto it = std::find_if(attendance.begin(), attendance.end(),
+        [&username](const UserAttendance &ua)
+        {
+            return ua.user->get_username() == username;
+        });
+
+    if (it == attendance.end())
+    {
+        std::cout << "User was not found in attendance records.\n";
+        return;
+    }
+
+    std::string date;
+
+    Input::get("Enter the date you want to edit (YYYY-MM-DD): ", date, DATE_MAX, Input::valid_date);
+
+    auto log_it = it->logs.find(date);
+    if (log_it == it->logs.end())
+    {
+        std::cout << "There is no attendance data for that date.\n";
+
+        string confirmation;
+        Input::get("Do you want to create attendance records for this date? [y/n]: ", confirmation, NUM_MAX, Input::valid_yes_or_no);
+
+        if (tolower(confirmation.at(0)) == 'n')
+        {
+            std::cout << "New records cancelled, returning to menu.\n";
+            return;
+        }
+
+        DailyAttendance new_log;
+        new_log.expected_login = Time::get_expected_login();
+        new_log.expected_logout = Time::get_expected_logout();
+
+        it->logs[date] = new_log;
+        log_it = it->logs.find(date);
+    }
+
+    DailyAttendance &log = log_it->second;
+
+    std::string input;
+    const int options = 5;
+
+    Input::get("What would you like to edit?\n"
+               "  1. Login Time\n  2. Logout Time\n  3. Expected Login\n"
+               "  4. Expected Logout\n  5. Exit\n"
+               "Enter the number of the option you want to perform: ", input, NUM_MAX, options, Input::valid_opt);
+    
+    int selection = std::stoi(input);
+
+    if (selection == 5)
+    {
+        std::cout << "Returning to menu.\n";
+        return;
+    }
+
+    std::string new_value;
+
+    Input::get("Enter the new value (HH:MM): ", new_value, LOGIN_MAX, Input::valid_log_time);
+
+    switch (selection)
+    {
+    case 1:
+        log.login_time = new_value;
+        break;
+
+    case 2:
+        log.logout_time = new_value;
+        break;
+
+    case 3:
+        log.expected_login = new_value;
+        break;
+
+    case 4:
+        log.expected_logout = new_value;
+        break;
+
+    default:
+        std::cerr << "Something has gone wrong, please try again later.\n";
+        break;
+    }
+
+    std::cout << "Attendance has been updated for " << username << " on " << date << ".\n";
 }
 
 void Admin::assign_roles(void)
 {
-    std::cout << "you are assigning roles.\n";
-    return;
-}
+    std::string username;
 
+    Input::get("Enter the username of the user to edit their role: ", username, USERNAME_MAX, Input::valid_username);
+
+    User *user = Storage::search_user_by_username(username);
+    if (!user)
+    {
+        std::cout << "User has not been found.\n";
+        return;
+    }
+
+    std::string input;
+    Roles role;
+    const int options = ROLES_COUNT - 1;
+
+    Input::get("Choose the new role:\n"
+               "  1. Admin\n  2. Manager\n  3. User\n"
+               "Enter the number of the role you want to assign: ", 
+               input, NUM_MAX, options, Input::valid_opt);
+    
+    role = (Roles) std::stoi(input);
+
+    user->set_role(role);
+
+    std::cout << user->get_role_name() << " privileges have been assigned to " << username << ".\n";
+}
 
 void User::mark_attendance(void)
 {
