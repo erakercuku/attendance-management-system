b89bbd8348ab0e5f342b1862b7b863acbc5276b9
haziel <144873984+itshazi@users.noreply.github.com>
2025-05-15 08:59:53 -0600

    Update main.cpp
    updated main login menu

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main(void)
     do 
     {
         Input::get("What would you like to do?\n"
-                   "  1. Login\n  2. Create an account\n  3. Exit\n"
+                   "  1. Login\n  2. Login with QR code\n  3. Create an account\n  4. Exit\n"
                    "Enter the number of the option you want to perform: ", 
                    input, NUM_MAX, options, Input::valid_opt);
 
@@ -46,6 +46,54 @@ int main(void)
         case LOGIN:
         {
             user = Auth::login();
+
+            if (user)
+            {
+                User* raw_user = insert_into_attendance(std::move(user));
+
+                #ifdef CSV_VER
+                    Storage::load_attendance();
+                #else
+                    Storage::load_attendance_from_db();
+                #endif
+
+                raw_user->mark_attendance();
+                raw_user->display_menu();
+
+                login_attempts = 0;
+
+                while (!raw_user->logout())
+                {
+                    raw_user->display_menu();
+                }
+
+                #ifdef CSV_VER
+                    Storage::save_attendance();
+                #else
+                    Storage::save_attendance_to_db();
+                #endif
+            }
+            else
+            {
+                login_attempts++;
+                if (login_attempts < LOGIN_ATTEMPTS_MAX)
+                {
+                    cout << "Attempts remaining: " << (LOGIN_ATTEMPTS_MAX - login_attempts) << '\n';
+                }
+                else
+                {
+                    cout << "Too many failed login attempts. Please wait a few minutes before attempting again.";
+                    std::this_thread::sleep_for(std::chrono::minutes(2));
+                }
+            }
+            break;
+        }
+        case LOGIN_QR:
+        {
+            cout << "Camera module functionality is currently not available. Please login manually.\n";
+            continue;
+
+            user = Auth::login_with_qr();
 
             if (user)
             {
